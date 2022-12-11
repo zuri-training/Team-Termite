c3150aeedbb1c4b02324034a492c556ea2244259
Adaobilynda1234 <adaobilynda1234@gmail.com>
2022-12-11 15:29:13 +0100

    model and auth

--- PriceAmRoot/PriceCompare/admin.py
+++ PriceAmRoot/PriceCompare/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(user)
+admin.site.register(product)
+admin.site.register(category)
+admin.site.register(Vendor)
+admin.site.register(comment)
+
