dba41c230108c46f93ba0fdc411024aed155fa75
daniel amodu <danielolayinka97@gmail.com>
2022-12-14 19:20:12 +0100

    hosting

--- PriceAmRoot/PriceAm/settings.py
+++ PriceAmRoot/PriceAm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e&(7&1ojr!d9icqihg-l29)@c=ac0&z!htuo$3fno1)p30hmoc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'PriceAm/static'),
     ]
